3d6730f86517693ed8d5f3ebbf5443fe1a3f71d5
Yağız <93614198+YagizSahin@users.noreply.github.com>
2023-01-27 16:56:50 +0300

    views and partial folders created

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const ejs= require('ejs');
+const path=require('path');
 
 const app = express();
-
+//template engine
+app.set("view engine","ejs");
+//middleware
+app.use(express.static('public'));
+//routes
 app.get('/',(req,res)=>{
-  const blog={
-    id:1,
-    title:"Blog Title",
-    description:"Blog description"
-  }
-  res.send(blog);
+  res.render('index');
+})
+app.get('/about',(req,res)=>{
+  res.render('about');
+})
+app.get('/add_post',(req,res)=>{
+  res.render('add_post');
+})
+app.get('/post',(req,res)=>{
+  res.render('post');
 })
 
 const port = 3000;
