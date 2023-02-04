8b575f8098ff14559b544c1fc7a199ca6453bd8b
Yağız <93614198+YagizSahin@users.noreply.github.com>
2023-02-05 01:22:57 +0300

    update, delete buttons and controllers created

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
-const Post= require('./models/Post');
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+const methodOverride = require("method-override");
+const Post = require("./models/Post");
+const mongoose = require("mongoose");
+const postControllers = require("./controllers/postControllers");
+const pageControllers = require("./controllers/pageControllers");
+const { assert } = require("console");
+mongoose.set("strictQuery", false);
 
 const app = express();
 //connect DB
-mongoose.connect('mongodb://127.0.0.1:27017/cleanBlog');
+mongoose.connect("mongodb://127.0.0.1:27017/cleanBlog");
 //template engine
 app.set("view engine", "ejs");
 //middleware
 app.use(express.static("public"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//routes
-app.get("/", async (req, res) => {
-  const posts=await Post.find({})
-  res.render("index",{
-    posts
-  });
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
-});
-app.get('/post/:id', async (req, res) => {
-  const post= await Post.findById(req.params.id);
-  res.render('post',{
-    post
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
   })
-});
-app.post('/add_post', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+);
+//routes
+app.get("/about", pageControllers.getAboutPage);
+app.get("/add_post", pageControllers.getAddPostPage);
+app.get("/post/edit/:id", pageControllers.getEditPage);
+app.get("/", postControllers.getAllPosts);
+app.get("/post/:id", postControllers.getPost);
+app.post("/add_post", postControllers.createPost);
+app.put("/post/:id", postControllers.updatePost);
+app.delete("/post/:id", postControllers.deletePost);
 
 const port = 3000;
 
---
+++ controllers/pageControllers.js
@@ -0,0 +1,13 @@
+const Post = require("../models/Post");
+exports.getAboutPage = (req, res) => {
+  res.render("about");
+};
+exports.getAddPostPage = (req, res) => {
+  res.render("add_post");
+};
+exports.getEditPage = async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render("edit", {
+    post,
+  });
+};
